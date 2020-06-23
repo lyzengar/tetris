1341182cd52b05595f9599f681fc840728893381
lyzengar <darthmaul85@hotmail.com>
2020-06-23 14:31:43 -0700

    add tetromino L array

--- tetris.js
+++ tetris.js
@@ -3,3 +3,9 @@ let squares = Array.from(document.querySelectorAll('.grid div'));
 const scoreDisplay = document.querySelector('#score');
 const startBtn = document.querySelector('#start-button');
 const width = 10;
+
+//The Tetrominoes
+
+const lTetromino = [
+    [1, width + 1, width * 2 + 1, 2],
+]
