7389b0648f6021e0e958ae37a70fa63b503b72e5
lyzengar <darthmaul85@hotmail.com>
2020-06-23 14:18:48 -0700

    clean up syntax

--- tetris.js
+++ tetris.js
@@ -1,4 +1,4 @@
-const grid = document.querySelector('.grid')
-let squares = Array.from(document.querySelectorAll('.grid div'))
-const scoreDisplay = document.querySelector('#score')
+const grid = document.querySelector('.grid');
+let squares = Array.from(document.querySelectorAll('.grid div'));
+const scoreDisplay = document.querySelector('#score');
 const width = 10;
