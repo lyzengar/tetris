131c9d3528f9c08895de6c0e7f9ff2e1b3667166
lyzengar <darthmaul85@hotmail.com>
2020-06-25 15:14:00 -0700

    add tetromino Z array

--- tetris.js
+++ tetris.js
@@ -11,4 +11,6 @@ const lTetromino = [
     [width, width + 1, width + 2, width * 2 + 2],
     [1, width + 1, width * 2 + 1, width * 2],
     [width, width * 2, width * 2 + 1, width * 2 + 2]
-]
+];
+
+const zTetromino = [];
