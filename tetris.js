ef79dd28ceca7d25a0d03c6af43870541fc6b48a
lyzengar <darthmaul85@hotmail.com>
2020-06-24 21:19:33 -0700

    add tetromino L array

--- tetris.js
+++ tetris.js
@@ -10,4 +10,5 @@ const lTetromino = [
     [1, width + 1, width * 2 + 1, 2],
     [width, width + 1, width + 2, width * 2 + 2],
     [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2]
 ]
